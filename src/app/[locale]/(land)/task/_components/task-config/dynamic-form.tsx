a46efeab791808a6a0d2d69df5de84ceaa4b615a
ilssn <ilssn@adswave.co>
2025-01-10 11:09:06 +0800

    upd: reset daynamic form

--- src/app/[locale]/(land)/task/_components/task-config/dynamic-form.tsx
+++ src/app/[locale]/(land)/task/_components/task-config/dynamic-form.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Trash2Icon } from "lucide-react";
+import { MagicWandIcon } from "@radix-ui/react-icons";
+import { MinusCircleIcon, PlusIcon, Trash2Icon } from "lucide-react";
 import * as React from "react";
 
 const DynamicForm = () => {
@@ -34,6 +35,10 @@ const DynamicForm = () => {
     setFormData(newFormData);
   };
 
+  const handleResetForm = () => {
+    setFormData([]);
+  };
+
   React.useEffect(() => {
     const formattedData = formData.reduce<Record<string, { type: string; description: string }>>((acc, field) => {
       acc[field.key] = {
@@ -49,43 +54,53 @@ const DynamicForm = () => {
     <div className="space-y-4">
       {formData.map((field, index) => (
         <div key={index} className="flex items-center space-x-1 space-y-1">
-          <Input
-            value={field.key}
-            onChange={(e) => handleKeyChange(e, index)}
-            placeholder="字段名称"
-            className="w-3/12"
-          />
-          <div className="w-3/12">
-            <Select
-              value={field.type}
-              onValueChange={(e) => handleTypeChange(e, index)}
+          <div className="flex w-full items-center space-x-1">
+            <Input
+              value={field.key}
+              onChange={(e) => handleKeyChange(e, index)}
+              placeholder="字段名称"
+              className="w-3/12"
+            />
+            <div className="w-3/12">
+              <Select
+                value={field.type}
+                onValueChange={(e) => handleTypeChange(e, index)}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="选择类型" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="string">string</SelectItem>
+                  <SelectItem value="number">number</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <Input
+              value={field.description}
+              onChange={(e) => handleDescriptionChange(e, index)}
+              placeholder="字段描述"
+              className="w-6/12"
+            />
+            <div
+              className="ml-2 cursor-pointer hover:scale-110 hover:text-red-600"
+              onClick={() => handleRemoveField(index)}
+              aria-label="删除字段"
             >
-              <SelectTrigger>
-                <SelectValue placeholder="选择类型" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="string">string</SelectItem>
-                <SelectItem value="number">number</SelectItem>
-              </SelectContent>
-            </Select>
+              <MinusCircleIcon className="h-4 w-4" />
+            </div>
           </div>
-          <Input
-            value={field.description}
-            onChange={(e) => handleDescriptionChange(e, index)}
-            placeholder="字段描述"
-            className="w-6/12"
-          />
-          <Button variant="ghost" size="sm" className="text-red-500 hover:text-red-600" onClick={() => handleRemoveField(index)}>
-            <Trash2Icon className="h-4 w-4" />
-          </Button>
         </div>
       ))}
+
+      <Button variant="ghost" onClick={handleAddField} className="w-full border">
+        <PlusIcon className="h-4 w-4" />
+      </Button>
       <div className="flex w-full justify-between">
-        <Button variant="outline" onClick={handleAddField}>
-          自动生成
+        <Button variant="outline" onClick={handleResetForm}>
+          <Trash2Icon className="h-4 w-4 text-red-600" />
         </Button>
         <Button variant="outline" onClick={handleAddField}>
-          添加字段
+          <MagicWandIcon className="h-4 w-4 text-primary" />
         </Button>
       </div>
     </div>
