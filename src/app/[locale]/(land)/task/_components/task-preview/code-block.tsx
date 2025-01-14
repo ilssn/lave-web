91ade4e056bd18795dc735247d4dbb93cce3024e
ilssn <ilssn@adswave.co>
2025-01-14 14:01:59 +0800

    upd: change code-block style

--- src/app/[locale]/(land)/task/_components/task-preview/code-block.tsx
+++ src/app/[locale]/(land)/task/_components/task-preview/code-block.tsx
@@ -1,6 +1,6 @@
 import beautify from 'js-beautify';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import * as themes from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const CodeBlock = ({ code, language }: { code: string, language: string }) => {
   // 根据语言格式化代码
@@ -9,9 +9,10 @@ const CodeBlock = ({ code, language }: { code: string, language: string }) => {
   return (
     <SyntaxHighlighter
       language={language}
-      style={themes.vscDarkPlus}
+      style={vscDarkPlus}
       showLineNumbers
-      lineNumberStyle={{ minWidth: '2em', textAlign: 'right', marginRight: '1em', opacity: 0.5 }}
+      codeTagProps={{ style: { fontSize: '14px', margin: '0' } }}
+      lineNumberStyle={{ minWidth: '2em', textAlign: 'right', opacity: 0.5 }}
     >
       {formattedCode}
     </SyntaxHighlighter>
